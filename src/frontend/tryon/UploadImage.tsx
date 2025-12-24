55f5b9be45495ec55a8296c33316497aad8ccc99
Aunshon <32583103+Aunshon@users.noreply.github.com>
2025-12-24 12:46:17 +0600

    Refactor UploadImage component: improve JSX structure, enhance accessibility, and streamline user image instructions.

--- src/frontend/tryon/UploadImage.tsx
+++ src/frontend/tryon/UploadImage.tsx
@@ -24,36 +24,32 @@ function UploadImage( {
 					className="hidden"
 				/>
 
-				<div>
-					{ userImages.length > 0 ? (
-						<ImagePreview
-							userImages={ userImages }
-							removeUserImage={ removeUserImage }
-						/>
-					) : (
-						<div className="rounded-[5px] border-2 border-dashed border-[#E0E0E0] bg-[#F8F9F8] px-8 py-12 text-center shadow-sm hover:border-gray-400 transition-colors duration-200">
-							<User
-								size={ 20 }
-								className="mx-auto text-[#828282]"
-							/>
+				{ userImages.length > 0 ? (
+					<ImagePreview
+						userImages={ userImages }
+						removeUserImage={ removeUserImage }
+					/>
+				) : (
+					<div className="rounded-[5px] border-2 border-dashed border-[#E0E0E0] bg-[#F8F9F8] px-8 py-12 text-center shadow-sm hover:border-gray-400 transition-colors duration-200">
+						<User size={ 20 } className="mx-auto text-[#828282]" />
 
-							<p className="mt-[6px] text-[14px] font-[600] text-primary">
-								{ __( 'Add Your Image', 'try-aura' ) }
-							</p>
+						<p className="m-0 mt-[6px] text-[14px] font-[600] text-primary">
+							{ __( 'Add Your Image', 'try-aura' ) }
+						</p>
 
-							<p className="mt-[6px] text-[12px] leading-[18px] font-[400] text-[#828282]">
-								{ __(
-									'Supports: jpg, png, and img formats.',
-									'try-aura'
-								) }
-								{ __(
-									'File size must be under 25 MB.',
-									'try-aura'
-								) }
-							</p>
-						</div>
-					) }
-				</div>
+						<p className="m-0 mt-[6px] text-[12px] leading-[18px] font-[400] text-[#828282]">
+							{ __(
+								'Supports: jpg, png, and img formats.',
+								'try-aura'
+							) }
+							<br />
+							{ __(
+								'File size must be under 25 MB.',
+								'try-aura'
+							) }
+						</p>
+					</div>
+				) }
 			</label>
 		</div>
 	);

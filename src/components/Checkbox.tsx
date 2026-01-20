e0479e16e720bec60cc89c593912f9b0491deb4a
Aunshon <32583103+Aunshon@users.noreply.github.com>
2026-01-20 16:30:06 +0600

    Refactor `Checkbox` styling for consistency and accessibility; enhance `VideoDetailsModal` with `label` for better usability.

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -17,12 +17,12 @@ const Checkbox = forwardRef< HTMLInputElement, CheckboxProps >(
 							ref={ ref }
 							type="checkbox"
 							className={ twMerge(
-								'col-start-1 row-start-1 appearance-none rounded-[2.73px] w-5 h-5 p-0 m-0 shadow-none border border-gray-300 bg-white checked:border-primary checked:bg-primary indeterminate:border-primary indeterminate:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 dark:border-white/10 dark:bg-white/5 dark:checked:border-primary dark:checked:bg-primary dark:indeterminate:border-primary dark:indeterminate:bg-primary dark:focus-visible:outline-primary dark:disabled:border-white/5 dark:disabled:bg-white/10 dark:disabled:checked:bg-white/10 forced-colors:appearance-auto cursor-pointer before:content-none',
+								'col-start-1 row-start-1 appearance-none rounded-[2.73px] w-5 h-5 p-0 m-0 shadow-none border bg-white not-checked:border-neutral-200 checked:border-primary checked:bg-primary indeterminate:border-primary indeterminate:bg-primary focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 dark:border-white/10 dark:bg-white/5 dark:checked:border-primary dark:checked:bg-primary dark:indeterminate:border-primary dark:indeterminate:bg-primary dark:focus-visible:outline-primary dark:disabled:border-white/5 dark:disabled:bg-white/10 dark:disabled:checked:bg-white/10 forced-colors:appearance-auto cursor-pointer before:content-none transition-all duration-200',
 								className
 							) }
 						/>
 						<Check
-							className="pointer-events-none col-start-1 row-start-1 size-4 self-center justify-self-center text-white opacity-0 group-has-checked:opacity-100 group-has-disabled:text-gray-950/25 dark:group-has-disabled:text-white/25"
+							className="pointer-events-none col-start-1 row-start-1 size-4 self-center justify-self-center text-white opacity-0 group-has-checked:opacity-100 group-has-disabled:text-gray-950/25 dark:group-has-disabled:text-white/25 transition-all duration-200"
 							size={ 7 }
 						/>
 					</div>

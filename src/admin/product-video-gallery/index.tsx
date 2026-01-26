8a0a56d1bc10e1c0e7405c2f54bdb9eaf68493fa
Aunshon <32583103+Aunshon@users.noreply.github.com>
2026-01-26 13:12:28 +0600

    Update `TryAura` product video gallery: add `group` class to wrapper div for improved hover styling consistency

--- src/admin/product-video-gallery/index.tsx
+++ src/admin/product-video-gallery/index.tsx
@@ -101,7 +101,7 @@ declare const tryAuraVideo: any;
 				}
 
 				$image.append( `
-					<div class="tryaura try-aura-product-video-wrapp">
+					<div class="tryaura try-aura-product-video-wrapp group">
 						<a href="#" class="try-aura-btn try-aura-product-gallery-video flex items-center justify-center bg-white/50 hover:bg-white text-white hover:text-primary no-underline ${ buttonClass }" data-attachment-id="${ attachmentId }">
 							<span class="hidden group-hover:flex text-[18px]! w-4.5! h-4.5! items-center! justify-center!">
 								<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-pencil-icon lucide-pencil"><path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/></svg>
